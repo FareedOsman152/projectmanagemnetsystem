afa015a5656bba43515d241ad04e558d5033cb1d
fareed <foxstar270@gmail.com>
2025-03-10 04:55:49 +0200

    Add new way to convert the object to xml

---
+++ ProjectTaskManagementSystem/Files/ObjectsConverter/UserConverter/UserConvertorXmlcs.cs
@@ -0,0 +1,65 @@
+﻿using ProjectTaskManagementSystem.UserSpace;
+using ProjectTaskManagementSystem.UserSpace.UserFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ProjectTaskManagementSystem.Files.ObjectsConverter.UserConverter;
+
+class UserConvertorXmlcs : IConvertor<User>
+{
+    private readonly UserFactory _userFactory;
+
+    public UserConvertorXmlcs(UserFactory userFactory)
+    {
+        _userFactory = userFactory;
+    }
+    public User ToObj(string xml)
+    {
+        var xmlSerializer = new XmlSerializer(typeof(User));
+        using (TextReader stringReader = new StringReader(xml))
+        {
+            return (User)xmlSerializer.Deserialize(stringReader);
+        }
+
+    }
+
+    public IEnumerable<User> ToObjs(IEnumerable<string> xmls)
+    {
+        var users = new List<User>();
+        foreach (var xml in xmls)
+        {
+            users.Add(ToObj(xml));
+        }
+        return users;
+    }
+
+    public string ToString(User obj)
+    {
+        string result;
+        var xmlSerlaizer = new XmlSerializer(obj.GetType());
+        using (var stringWriter = new StringWriter())
+        {
+            using (var xmlWriter = XmlWriter.Create(stringWriter, new XmlWriterSettings { Indent = true}))
+            {
+                xmlSerlaizer.Serialize(xmlWriter, obj);
+                result = stringWriter.ToString();
+            }
+        }
+        return result;
+    }
+
+    public IEnumerable<string> ToStrings(IEnumerable<User> objs)
+    {
+        var xmls = new List<string>();
+        foreach (var obj in objs)
+        {
+            xmls.Add(ToString(obj));
+        }
+        return xmls;
+    }
+}
--- ProjectTaskManagementSystem/obj/Debug/net8.0/ProjectTaskManagementSystem.AssemblyInfo.cs
+++ ProjectTaskManagementSystem/obj/Debug/net8.0/ProjectTaskManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectTaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e9cba4149e19bba22ea4e4bcb1c2c5420ea55a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+007c354ae30db3b666efd79ea871f7998d0af7a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectTaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectTaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
